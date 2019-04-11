acc85faaf5bd6c5bc614499d3dd19ee369afc40c
Chelsea Yue <cyue@lcs.on.ca>
2019-04-10 21:32:04 -0400

    changed start screen

--- FlappyAlpaca/StartScreen.java
+++ FlappyAlpaca/StartScreen.java
@@ -17,8 +17,12 @@ public class StartScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+        
+       
 
         prepare();
+
+        
     }
 
     public void prepare() 
